3a50d4f98009cf8f6c03f1ad2af44de500286e84
i998979 <brianx2x@gmail.com>
2025-04-06 18:35:56 +0800

    Added missing supported inferencing language

--- GPT_SoVITS/inference_cli.py
+++ GPT_SoVITS/inference_cli.py
@@ -41,9 +41,11 @@ def main():
     parser.add_argument('--sovits_model', required=True, help="Path to the SoVITS model file")
     parser.add_argument('--ref_audio', required=True, help="Path to the reference audio file")
     parser.add_argument('--ref_text', required=True, help="Path to the reference text file")
-    parser.add_argument('--ref_language', required=True, choices=["中文", "英文", "日文"], help="Language of the reference audio")
+    parser.add_argument('--ref_language', required=True,
+                        choices=["中文", "英文", "日文", "粤语", "中英混合", "日英混合", "粤英混合", "韩英混合", "多语种混合", "多语种混合(粤语)"], help="Language of the reference audio")
     parser.add_argument('--target_text', required=True, help="Path to the target text file")
-    parser.add_argument('--target_language', required=True, choices=["中文", "英文", "日文", "中英混合", "日英混合", "多语种混合"], help="Language of the target text")
+    parser.add_argument('--target_language', required=True,
+                        choices=["中文", "英文", "日文", "粤语", "中英混合", "日英混合", "粤英混合", "韩英混合", "多语种混合", "多语种混合(粤语)"], help="Language of the target text")
     parser.add_argument('--output_path', required=True, help="Path to the output directory")
 
     args = parser.parse_args()
